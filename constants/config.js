b24677fa23e838b2c518341c7d01c42feea5a87d
Priyanga2024 <priyangam@traya.health>
2025-05-15 16:27:59 +0530

    remve the cahnges of config

--- constants/config.js
+++ constants/config.js
@@ -1,15 +1,6 @@
-export const PUBLIC_API_URL_BASE = "https://public-zxhj2.dev.hav-g.in/";
-export const COOKIES_DOMAIN = "vayuskin.in";
-export const CDN_BASE_URL = "https://d3twoz8hvcdm9h.cloudfront.net/";
-
-//PROD
-
-// export const SECURITY_TOKEN = "974536e0-3f0c-4632-b1e4-c6a4c46b7c4f";
-// export const DEFAULT_API_URL =
-//   "https://api-gateway-thnu3.hav-g.in/";
-
-//DEV
-
 export const SECURITY_TOKEN = "e2623576-930b-48b6-81e2-a3cb5e37f47d";
 export const DEFAULT_API_URL =
   "https://consumer-api-gateway-tnutsdr7.dev.hav-g.in/";
+export const PUBLIC_API_URL_BASE = "https://public-zxhj2.dev.hav-g.in/";
+export const COOKIES_DOMAIN = "vayuskin.in";
+export const CDN_BASE_URL = "https://d3twoz8hvcdm9h.cloudfront.net/";
