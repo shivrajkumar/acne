e1f47954af0d42a3546eef12afda13701060f658
Priyanga2024 <priyangam@traya.health>
2025-06-09 12:01:36 +0530

    submit api is added

--- components/form/InputImage.jsx
+++ components/form/InputImage.jsx
@@ -9,12 +9,13 @@ import { useRouter, useSearchParams } from "next/navigation";
 // import selfie from "@assets/images/selfie.png";
 import Image from "next/image";
 import { useEffect } from "react";
+import { formFillStatus } from "@/enums/QuestionEnums";
 import { CDN_BASE_URL } from "@constants/config";
 // import { getCurrentTimeInReadableForm } from "@/helpers/timeFormatter";
 // import { sendMoengageEvent } from "@/helpers/handleMoengage";
 import CameraAccess from "../inputComponents/cameraCapture/CameraAccess";
 import { fetchRequest } from "@/helpers/fetchRequest";
-import { IMAGE_UPLOAD_API } from "@/constants/urls";
+import { IMAGE_UPLOAD_API, TRANSACTION_API } from "@/constants/urls";
 
 const settingIcon = `${CDN_BASE_URL}website_images/localImages/setting_icon.webp`;
 const front_view =  `${CDN_BASE_URL}website_images/clear_rituals/skin_test/acne_upload.webp`
@@ -23,7 +24,7 @@ const InputImage = ({ block }) => {
   const {
     saveReply,
     setAllQuestionsFilled,
-    apiResponse: { caseId },
+    apiResponse: { caseId ,transactionId },
   } = useContext(QuestionsContext);
 
   const handleSubmit = useFormSubmit(QuestionsContext);
@@ -173,8 +174,38 @@ const InputImage = ({ block }) => {
 
         const _res = await fetchRequest(IMAGE_UPLOAD_API(caseId), _options);
         if (_res?.success || _res?.status === 200) {
-          handleSubmit(reply);
+
+             const _formData = {
+                    question_id: block.id,
+                    field_key: block.id,
+                    question_text: block.text,
+                    response: reply,
+                    status:
+                      block.id == "photo_q"
+                        ? formFillStatus.FILLED
+                        : formFillStatus.SEMI_FILLED,
+                    location_path: window.location.pathname + window.location.search,
+                    source: "website",
+                    response_type: block.type,
+                  };
+            
+                  const _options = {
+                    method: "POST",
+                    body: JSON.stringify(_formData),
+                  };
+            
+                  if (["customer_values"].includes(block.next)) {
+                    window.localStorage.setItem("form_status", "semi-filled");
+                  }
+            
+                  const response = await fetchRequest(TRANSACTION_API(transactionId), _options);
+                  console.log(response ,"response")
+                  if(response.status==200){
+                     handleSubmit(reply);
+       
           setAllQuestionsFilled(true);
+
+                  }  
           window.localStorage.setItem("form_status", "filled");
         } else {
           setErr(_res?.message || "Image upload failed. Please try again.");
