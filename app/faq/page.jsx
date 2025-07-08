80811556f72c2d3401e0beab515526eac5e4ae55
Priyanga2024 <priyangam@traya.health>
2025-07-08 14:29:07 +0530

    banner made

--- app/faq/page.jsx
+++ app/faq/page.jsx
@@ -1,14 +1,9 @@
-import { Suspense } from "react";
-import AcneMarqueeBanner from "@/components/generic/AcneMarqueeBanner";
-import AcneHeader from "@/components/generic/AcneHeader";
-import AcneFooter from "@/components/generic/AcneFooter";
+import AcneFAQPage from "@/components/faq/AcneFAQ";
 
 export default function page() {
   return (
     <>
-      <Suspense>
-     
-      </Suspense>
+      <AcneFAQPage />
     </>
   );
 }
