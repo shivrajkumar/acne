86464baedac9db59b4890d6775f0de784370dc01
SiriYadav-S <siriyadav@traya.health>
2025-07-10 15:44:37 +0530

    beforeSendHandler function call

--- components/generic/UmamiTracker.jsx
+++ components/generic/UmamiTracker.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+"use client"
+import { useEffect } from 'react';
 
-export default async function UmamiTracker() {
+export default function UmamiTracker() {
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            window.beforeSendHandler = function (type, payload) {
+                // console.log("TRACKER PLOAD", type, payload);
+                return payload;
+            };
+        }
+    }, []);
+
     return (
         <script
-            defer src="https://umami.dev.hav-g.in/script.js"
+            defer
+            src="https://umami.dev.hav-g.in/script.js"
             data-website-id="5f55e752-9b42-4a0f-b664-fb92a77353e6"
             data-before-send="beforeSendHandler">
         </script>
-    )
-}
-
-function beforeSendHandler(type, payload) {
-    // todo: if local storage contains case id then payload.id = caseId
-    console.log("TRACKER PLOAD", type, payload);
-    return payload;
+    );
 }
