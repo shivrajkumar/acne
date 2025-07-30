ebf0c740a0f3eef3e17ebfed738142ee93c85ca8
Anuragx98 <anuragchidanand@traya.health>
2025-07-30 12:06:40 +0530

    changes to test before prod push.

--- app/faq/layout.jsx
+++ app/faq/layout.jsx
@@ -1,9 +1,24 @@
+'use client'
 import AcneFooter from "@/components/generic/AcneFooter";
 import AcneHeader from "@/components/generic/Header/AcneHeader";
 import AcneMarqueeBanner from "@/components/generic/AcneMarqueeBanner";
 import React from "react";
+import useMediaLoader from "@/hooks/useMediaLoader";
+import Loader from "@/components/generic/Loader";
 
 const Layout = ({ children }) => {
+  const isLoading = useMediaLoader({
+    minLoadingTime: 2000,
+    maxLoadingTime: 3000,
+    transitionDelay: 1500
+  });
+
+  // Show loader while loading
+  if (isLoading) {
+    return <Loader />;
+  }
+  
+  
   return (
     <div>
       <AcneMarqueeBanner />
