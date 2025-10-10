b4f7b33b25541118769291d6bb2bc3f849b32265
Anuragx98 <anuragchidanand@traya.health>
2025-10-10 11:59:38 +0530

    grading showing without level keyword.

--- components/result/ResultBannerV2.jsx
+++ components/result/ResultBannerV2.jsx
@@ -18,26 +18,14 @@ const progressMapping = {
 };
 
 const ResultBannerV2 = () => {
-  const { customerDetails, skinType, acneGrading } =
-    useCartContext();
+  const { customerDetails, grading } =useCartContext();
 
-  const acneProgress = useMemo(() => {
-    const condition = acneGrading?.split(" ")[2]?.toLowerCase().trim();
-    return progressMapping[condition] || 0;
-  }, [acneGrading]);
-
-  const skinTypeProgress = useMemo(() => {
-    const condition = skinType?.split("+")[0]?.toLowerCase().trim();
-    return progressMapping[condition] || 0;
-  }, [skinType]);
 
   const customerName = useMemo(
     () =>
       customerDetails?.firstName ? startCase(customerDetails.firstName) : "",
     [customerDetails]
   );
-  const formattedSkinType = skinType?.split("+");
-  const formattedAcneGrading = acneGrading?.split(" ");
 
   return (
     <div className="w-full overflow-hidden bg-Secondary/50  rounded-[1px] md:rounded-[12px] mt-[16px] sm:mt-[16px] md:mt-[32px] flex flex-col md:flex-row justify-between gap-[40px] md:gap-[120px]">
@@ -47,7 +35,7 @@ const ResultBannerV2 = () => {
             Hi, {customerName}
           </h1>
           <h1 className="text-[24px] md:text-[40px] font-sophiaPro font-light text-Grey/900 leading-[1.3] md:w-[600px] w-full">
-            You have <span className="font-semibold">{acneGrading} acne</span>
+            You have <span className="font-semibold">{grading} acne</span>
           </h1>
         </div>
       </div>
